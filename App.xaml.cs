ac700e56ed871191e50adb3a57985e749ee5c4ca
Olteanu Flavius <olteanuflavius663@gmail.com>
2024-11-30 12:40:49 +0200

    Lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,25 @@
-﻿namespace OlteanuFlaviusLab7
+﻿using System;
+using OlteanuFlaviusLab7.Data;
+using System.IO;
+
+namespace OlteanuFlaviusLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
